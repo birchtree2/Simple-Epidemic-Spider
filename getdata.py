c778fef0289c05ad044942dd82e0268516096c33
GaisaiYuno <steven_mhy@outlook.com>
2020-06-28 11:17:21 +0800

    无可视化

---
+++ getdata.py
@@ -0,0 +1,25 @@
+#encode: utf-8
+import requests
+import time,sys,os
+import configparser
+from urllib import request
+from urllib import parse
+def parse(data):
+    print("地区名："+data["name"])
+    updateDate=data["trend"]["updateDate"]
+    last=len(updateDate)-1
+    date=updateDate[last]
+    print("日期："+date)
+    for i in data["trend"]["list"]:
+        #print(i)
+        lst=len(i["data"])-1
+        print(i["name"]+":"+(str)(i["data"][lst]))
+    print("-------")
+        
+if __name__=="__main__":
+    url = "https://voice.baidu.com/newpneumonia/get?target=trend&isCaseIn=0&stage=publish"
+    response=requests.post(url)
+    data=response.json()
+    print(data['msg'])
+    for i in data["data"]:
+        parse(i)
