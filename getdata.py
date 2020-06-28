04df00f6aced78bdafcfaa12977e88dac822b359
birchtree2 <1980878786@qq.com>
2020-06-28 11:27:02 +0800

    修正变量名重复错误

--- getdata.py
+++ getdata.py
@@ -1,10 +1,11 @@
 #encode: utf-8
 import requests
+import numpy
 import time,sys,os
 import configparser
 from urllib import request
 from urllib import parse
-def parse(data):
+def parse1(data):
     print("地区名："+data["name"])
     updateDate=data["trend"]["updateDate"]
     last=len(updateDate)-1
@@ -15,11 +16,11 @@ def parse(data):
         lst=len(i["data"])-1
         print(i["name"]+":"+(str)(i["data"][lst]))
     print("-------")
-        
+
 if __name__=="__main__":
     url = "https://voice.baidu.com/newpneumonia/get?target=trend&isCaseIn=0&stage=publish"
     response=requests.post(url)
     data=response.json()
     print(data['msg'])
     for i in data["data"]:
-        parse(i)
+        parse1(i)
