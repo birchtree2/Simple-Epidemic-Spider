d57d4bd9c3da3dfa9eb1c516d68217379733db4f
Steven Meng <52853137+GaisaiYuno@users.noreply.github.com>
2020-07-01 17:33:47 +0800

    update

--- numpy_make_can_see.py
+++ numpy_make_can_see.py
@@ -7,31 +7,31 @@ from urllib import request
 from urllib import parse
 import matplotlib.pyplot as plt
 
-def show(x,y):
-    plt.rcParams['font.sans-serif'] = 'SimHei'
-    plt.rcParams['axes.unicode_minus'] = False
-    plt.figure()
-    plt.bar(x, y)
+def make_graph(t,x,y):
+    plt.rcParams['font.sans-serif']='SimHei'
+    plt.rcParams['axes.unicode_minus']=False
+    #plt.figure()
+    plt.title(t)
+    plt.bar(x,y,linewidth=2.0,linestyle='--')
+    plt.legend()
     plt.show()
 
-def parse1(data):
+def process(data):
+    """
+    Process data , output date and show graph
+    """
     print("地区名："+data["name"])
     updateDate=data["trend"]["updateDate"]
-    last=len(updateDate)-1
-    date=updateDate[last]
-    print("日期："+date)
+    first,last=0,len(updateDate)-1
+    print("日期："+updateDate[first]+" -> "+updateDate[last])
     for i in data["trend"]["list"]:
-        #print(i)
-        #lst=len(i["data"])-1
-        #print(i["name"]+":"+(str)(i["data"][lst]))
-        show(updateDate,i["data"])
-
+        make_graph(i["name"],updateDate,i["data"])
     print("-------")
 
 if __name__=="__main__":
-    url = "https://voice.baidu.com/newpneumonia/get?target=trend&isCaseIn=0&stage=publish"
+    url="https://voice.baidu.com/newpneumonia/get?target=trend&isCaseIn=0&stage=publish"
     response=requests.post(url)
     data=response.json()
     print(data['msg'])
     for i in data["data"]:
-        parse1(i)
+        process(i)
